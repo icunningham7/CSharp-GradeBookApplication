a831b75a7970b0076b3139791c9150feb6794931
icunningham7 <ian.cunningham@Moody.edu>
2021-04-20 10:56:44 -0500

    I.X

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -8,9 +8,10 @@ namespace GradeBook.GradeBooks
 {
     public class RankedGradeBook : BaseGradeBook
     {
-        public RankedGradeBook(string name) : base(name)
+        public RankedGradeBook(string name, bool isWeighted) : base(name, isWeighted)
         {
             Name = name;
+            IsWeighted = isWeighted;
             Type = GradeBookType.Ranked;
         }
 
