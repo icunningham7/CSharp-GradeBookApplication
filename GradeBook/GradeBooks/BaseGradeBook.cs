f5e74e0540541999a46b75a64b807337ee3514aa
icunningham7 <ian.cunningham@Moody.edu>
2021-04-20 11:10:10 -0500

    I.X.II

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -113,23 +113,18 @@ namespace GradeBook.GradeBooks
             switch (letterGrade)
             {
                 case 'A':
-                    return 4;
                     gpa = 4;
                     break;
                 case 'B':
-                    return 3;
                     gpa = 3;
                     break;
                 case 'C':
-                    return 2;
                     gpa = 2;
                     break;
                 case 'D':
-                    return 1;
                     gpa = 1;
                     break;
                 case 'F':
-                    return 0;
                     gpa = 0;
                     break;
             }
