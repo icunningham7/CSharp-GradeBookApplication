fd35d2a532fb5fe9fb03e5f9c59e37b1a6997302
icunningham7 <ian.cunningham@Moody.edu>
2021-04-20 09:20:57 -0500

    I.III

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -13,17 +13,14 @@ namespace GradeBook.GradeBooks
     {
         public string Name { get; set; }
         public List<Student> Students { get; set; }
-
+        public GradeBookType Type { get; set; }
         public BaseGradeBook(string name)
         {
             Name = name;
             Students = new List<Student>();
         }
 
-        public GradeBookType Type 
-        {
-            get; set;
-        }
+        
         public void AddStudent(Student student)
         {
             if (string.IsNullOrEmpty(student.Name))
