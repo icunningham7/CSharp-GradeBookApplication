a6fae7a626cd6068dfbd2adbea1c9ca8d8269ab9
icunningham7 <ian.cunningham@Moody.edu>
2021-04-20 10:57:59 -0500

    I.X isWeighted fix

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -41,8 +41,7 @@ namespace GradeBook.UserInterfaces
             }
             var name = parts[1];
             var type = parts[2].ToLower();
-            var 
-                = bool.Parse(parts[3]);
+            var isWeighted = bool.Parse(parts[3]);
 
             BaseGradeBook gradeBook;
             if (type == "standard")
