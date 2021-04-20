29cee0b5bc2e5462faa8efc3327cce45d617fe14
icunningham7 <ian.cunningham@Moody.edu>
2021-04-20 10:04:20 -0500

    I.V

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -16,7 +16,7 @@ namespace GradeBook.GradeBooks
 
         public override char GetLetterGrade(double averageGrade)
         {
-            if (Students.Count < 5)
+            if (Students.Count() < 5)
 
             {
                 throw new InvalidOperationException("You must have at least 5 students to do ranked grading.");
@@ -37,7 +37,7 @@ namespace GradeBook.GradeBooks
         }
         public override void CalculateStatistics()
         {
-            if (Students.Count < 5)
+            if (Students.Count() < 5)
             {
                 Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
                 return;
@@ -47,7 +47,7 @@ namespace GradeBook.GradeBooks
         }
         public override void CalculateStudentStatistics(string name)
         {
-            if (Students.Count < 5)
+            if (Students.Count() < 5)
             {
                 Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
                 return;
